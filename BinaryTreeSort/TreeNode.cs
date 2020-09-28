92a0c36233b9c51b0281545ee1e7dc5580de49c7
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-09-28 19:34:18 +0530

    Add project files.

---
+++ BinaryTreeSort/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeSort
+{
+    class TreeNode
+    {
+        public TreeNode lchild;
+        public char info;
+        public TreeNode rchild;
+        public TreeNode(char ch)
+        {
+            info = ch;
+            lchild = null;
+            rchild = null;
+        }
+    }
+}
